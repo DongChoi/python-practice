f89f470e101571aaf927fa8d3926e796e49edb86
Dong Hyun Choi <dongandrewchoi@gmail.com>
2022-03-21 19:02:12 -0700

    Thanks for the comprehesion lesson!

--- 16_partition.py
+++ 16_partition.py
@@ -19,3 +19,14 @@ def partition(lst, fn):
         >>> partition(["hi", None, 6, "bye"], is_string)
         [['hi', 'bye'], [None, 6]]
     """
+    passed = []
+    failed = []
+    result = []
+
+    [passed.append(item) if fn(item) else failed.append(item) for item in lst]
+    
+    result.append(passed)
+    result.append(failed)
+    return result
+
+    
